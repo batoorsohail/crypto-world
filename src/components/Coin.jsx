4d120bd0a75ef29041dd8ea29924ebd9dba56931
unknown <sohailshahidi1435@gmail.com>
2023-03-30 16:31:22 +0430

    Add search input

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getCryptoCoinsAsync } from '../redux/coin/cryptoSlice';
 
 export default function Home() {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState('');
+
   useEffect(() => {
     dispatch(getCryptoCoinsAsync());
   }, [dispatch]);
@@ -15,21 +17,37 @@ export default function Home() {
     return <p>No coins loaded</p>;
   }
 
+  const searchCoins = cryptoCoins
+    .filter((crypto) => crypto.name.toLowerCase()
+      .includes(searchQuery.toLowerCase()));
+
   return (
-    <section>
-      {
-        cryptoCoins.map((cryptoCoin) => (
-          <article key={cryptoCoin.id}>
-            <Link to={`crypto-world/details/${cryptoCoin.id}`}>Details</Link>
-            <img
-              src={cryptoCoin.icon}
-              alt={`${cryptoCoin.name}logo`}
-            />
-            <h2>{cryptoCoin.name}</h2>
-            <span>{cryptoCoin.totalSupply}</span>
-          </article>
-        ))
-      }
-    </section>
+    <>
+      <section>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </section>
+      <section>
+        {
+          searchCoins.map((cryptoCoin) => (
+            <article key={cryptoCoin.id}>
+              <Link to={`crypto-world/details/${cryptoCoin.id}`}>Details</Link>
+              <img
+                src={cryptoCoin.icon}
+                alt={`${cryptoCoin.name}logo`}
+              />
+              <h2>{cryptoCoin.name}</h2>
+              <span>{cryptoCoin.totalSupply}</span>
+            </article>
+          ))
+        }
+      </section>
+    </>
   );
 }
+
+
